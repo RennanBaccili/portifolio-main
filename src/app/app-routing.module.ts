33e143c847e3c986a0b682e75ec1694e7be1bd02
Rennan Bacili <rennanbaccili@gmail.com>
2023-09-21 16:22:34 -0300

    projeto portifólio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+//rotas
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'projects', component:ProjectsComponent, pathMatch:'full'},
+  {path:'about', component:AboutComponent ,pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './pages/home/main/main.component';
+import { ButtonsComponent } from './pages/home/main/buttons/buttons.component';
+import { IconsComponent } from './pages/home/main/icons/icons.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { AboutComponent } from './pages/about/about.component';
+
+import { HomeModule } from './pages/home/home.module';
+import { ProjectsModule } from './pages/projects/projects.module';
+import { AboutModule } from './pages/about/about.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    MainComponent,
+    ButtonsComponent,
+    IconsComponent,
+    ProjectsComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    ProjectsModule,
+    AboutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
